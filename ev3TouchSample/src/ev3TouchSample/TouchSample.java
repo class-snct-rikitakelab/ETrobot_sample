92cda7ac3d6ee2fac95b4b8d43beb4e472aa35e7
yusan <s1000614@sendai-nct.jp>
2015-04-19 17:47:36 +0900

    Renew Touchsample.java  Renew a method main  Add methods setTSensor CheckTSensor LCDclear drawLCD

--- ev3TouchSample/src/ev3TouchSample/TouchSample.java
+++ ev3TouchSample/src/ev3TouchSample/TouchSample.java
@@ -4,34 +4,74 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.SensorMode;
+import lejos.utility.Delay;
 
 public class TouchSample {
 
 	//使用するセンサーを定義する
 	//タッチセンサーはS2ポートにつながっているためこのように定義する
 	static EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S2);
+	static SensorMode touch;
 
 	public static void main(String[] args) {
-		//センサーモードを設定する
-		SensorMode touch = touchSensor.getMode(0);
 
-		//センサーが取得する値を格納する配列を用意する
-		float value[] = new float[touch.sampleSize()];
-
-		//LCDを初期化する
-		LCD.clear();
+		//センサーの初期設定
+		setTSensor();
 
 		while(true){
-			//センサーが取得する値を配列に格納する
-			touch.fetchSample(value, 0);
 
-			//もしセンサーが押されていたらLCDに表示する
-			if(value[0]==1){LCD.drawString("Touch", 1, 1);}
-			//センサーが押されていないならLCD表示を消す
-			else{LCD.clearDisplay();}
+			//LCD画面をクリアする
+			lcdclear();
 
-			LCD.asyncRefresh();
+			//もしタッチセンサーが押されているかそうでないかを判定
+			if(CheckTSensor()==true){
+				//もしタッチセンサーが押されていたらLCDにTouchedと表示
+				drawLCD("Touched",0,4);
+			}else{
+				//もしタッチセンサが押されていなかったらLCDにNot Touchedと表示
+				drawLCD("Not Touched",0,4);
+			}
+
+			//0.5秒間待機
+			Delay(500);
 		}
 	}
 
+
+////////////////////////////////////////////////////////////////////////
+//ここから下は各関数を記しておきます．
+
+	//タッチセンサの初期設定を行います．タッチセンサを使うときはこれをまず宣言してください．
+	public static void setTSensor(){
+		touch = touchSensor.getMode(0);
+	}
+
+	//タッチセンサが押されているかどうかを判定します．押されていたらtrueを，押されていなかったらfalseを返します．
+	public static boolean CheckTSensor(){
+		float value[] = new float[touch.sampleSize()];
+
+		touch.fetchSample(value, 0);
+
+		if(value[0]==1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	//LCDの表示をすべて消します．
+	private static void  lcdclear(){
+		LCD.refresh();
+		LCD.clear();
+	}
+
+	//LCDに好きな文章を表示できます．
+	//第1の引数はメッセージの文字列，第2の引数はメッセージの1文字目のx座標，第3の引数はy座標です．
+	private static void drawLCD(String message,int x,int y){
+		LCD.drawString(message, x, y);
+	}
+
+	private static void Delay(int msec){
+		Delay.msDelay(msec);
+	}
 }
